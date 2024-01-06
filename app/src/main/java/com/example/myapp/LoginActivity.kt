ace9b122c5661395eef2376c99f24a6b0648bd35
aktasduygu <110852592+aktasduygu@users.noreply.github.com>
2024-01-06 16:05:59 +0300

    My App

---
+++ app/src/main/java/com/example/myapp/LoginActivity.kt
@@ -0,0 +1,50 @@
+package com.example.myapp
+
+import android.content.Intent
+import android.graphics.drawable.ColorDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.myapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.loginEmail.text.toString()
+            val password = binding.loginPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+            }
+        }
+        binding.signupRedirectText.setOnClickListener {
+            val signupIntent= Intent(this,SignupActivity::class.java)
+            startActivity(signupIntent)
+            }
+    }
+}
---
+++ crudadmin/src/main/java/com/example/crudadmin/UpdateActivity.kt
@@ -0,0 +1,44 @@
+package com.example.crudadmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.crudadmin.databinding.ActivityUpdateBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class UpdateActivity : AppCompatActivity() {
+
+
+    private lateinit var binding: ActivityUpdateBinding
+    private lateinit var database: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.updateButton.setOnClickListener {
+            val referencePhone = binding.referencePhone.text.toString()
+            val updateName = binding.updateName.text.toString()
+            val updateOperator = binding.updateOperator.text.toString()
+            val updateLocation = binding.updateLocation.text.toString()
+            updateData(referencePhone,updateName,updateOperator,updateLocation)
+        }
+    }
+    private fun updateData(phone: String, name: String, operator: String, location: String) {
+        database = FirebaseDatabase.getInstance().getReference("Users")
+        val user = mapOf<String,String>(
+            "name" to name,
+            "operator" to operator,
+            "location" to location
+        )
+        database.child(phone).updateChildren(user).addOnSuccessListener {
+            binding.referencePhone.text.clear()
+            binding.updateName.text.clear()
+            binding.updateOperator.text.clear()
+            binding.updateLocation.text.clear()
+            Toast.makeText(this,"Successfully Updated",Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener{
+            Toast.makeText(this,"Failed to Update",Toast.LENGTH_SHORT).show()
+        }}
+}
